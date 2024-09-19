4b8b5b8ef735ef7dea157a86052aad0b656ede47
Yanik Maurer <yanik.maurer@outlook.com>
2024-09-19 13:07:49 +0200

    Update RunbookCall.ps1

--- runbook/RunbookCall.ps1
+++ runbook/RunbookCall.ps1
@@ -1,30 +1,39 @@
 # Define the WebHook URL
-$webhookUrl = "https://40d31616-3614-4b2f-ab7b-b88b27f680a7.webhook.stzn.azure-automation.net/webhooks?token=2mFGOVhupAm9Hp7xreR479XHYIXu%2bGMlllLD3iDCrfI%3d"
-$APIKey = "kt+a9euHJBp-j%e2*NE*FT8WeVQcsrKnO-g--3cuDZ8w9U%7wMb-&bnBvCSQo1?W-2i2z7UN7*0dK@gWW0Zgr_W#3ReRN&5hXZElcjOWPbIuKkmN0Iu@VKsD=VDUIUnY"
+$webhookUrl = ""
+$userAgent = ""
+$clientId = "" 
+$APIKey = ""
 
 # Define the payload 
 $payload = @{
-    "APIKey" = $APIKey
-    "Company" = "Speedgoat"
+    "Company" = "Company"
     "EmployeeId" = "5634"
     "Firstname" = "Hans"
     "Lastname" = "Wurscht"
     "Username" = "hans.wurscht"
-    "Department" = "Corp Dev"
+    "Department" = "testdept"
     "Team" = "IT"
     "JobTitle" = "Spezi"
-    "Manager" = "Yanik.Maurer"
-    "onboardingDate" = "2024-09-18"
-    "Telephone" = "0791901245"
+    "Manager" = "peter.wurscht"
+    "onboardingDate" = "2024-09-20"
+    "Telephone" = "1234525"
     "CountryCode" = "CH"
-    "City" = "Liebefeld"
-    "OfficePhone" = "079123414"
-    "StreetAddress" = "Strasse"
-    "ZipCode" = "3762"
+    "City" = "City"
+    "OfficePhone" = "1231312"
+    "StreetAddress" = "Street"
+    "ZipCode" = "12334"
 }
 
-# Convert the payload to JSON
 $payloadJson = $payload | ConvertTo-Json
 
-# Send the POST request
-Invoke-RestMethod -Uri $webhookUrl -Method POST -Body $payloadJson -ContentType "application/json"
+# Define custom headers
+$headers = @{
+    "X-APIKey" = $APIKey   
+    "X-Timestamp" = ((Get-Date).ToString("o"))  # ISO 8601 format
+    "X-ClientId" = $clientId            
+    "User-Agent" = $userAgent                  
+}
+
+# Send the POST request with custom headers
+Invoke-RestMethod -Uri $webhookUrl -Method POST -Body $payloadJson -ContentType "application/json" -Headers $headers
+
